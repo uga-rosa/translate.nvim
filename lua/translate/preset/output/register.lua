4d6cc190eba5e49191f0ee2ffa7995157658cedd
uga-rosa <uga6603@gmail.com>
2022-05-13 06:49:40 +0900

    Fix: output/register respect fileformat for newline

--- lua/translate/preset/output/register.lua
+++ lua/translate/preset/output/register.lua
@@ -5,7 +5,17 @@ local M = {}
 ---Set the register
 ---@param lines string[]
 function M.cmd(lines, _)
-    local text = table.concat(lines, "")
+    local newline
+    local ff = vim.o.fileformat
+    if ff == "unix" then
+        newline = "\n"
+    elseif ff == "dos" then
+        newline = "\r\n"
+    else
+        newline = "\r"
+    end
+
+    local text = table.concat(lines, newline)
 
     local options = require("translate.config").get("preset").output.register
     local name = options.name
